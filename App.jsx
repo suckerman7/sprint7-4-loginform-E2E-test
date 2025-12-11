9353638ae61287a5c177b2567a9778a533b3902c
Suckerman7 <mrty.yksl@gmail.com>
2025-12-11 19:02:00 +0300

    Component'lar eklendi
    App ve Success sayfası eklendi, Login sayfası üzerinde uğraşıyorum

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Login from './Login';
+import Success from './Success';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+    return (
+        <div className='content-section'>
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={Login}>
+                    </Route>
+                    <Route exact path='/main' component={Success}>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
+}
---
+++ Success.jsx
@@ -0,0 +1,12 @@
+export default function Success() {
+    return (
+        <div style={{
+            textAlign: "center",
+            marginTop: "80px",
+            fontFamily: "Arial"
+        }}>
+            <h1 style={{ color: "green" }}>✔ Login Successful!</h1>
+            <p>Welcome, you have successfully logged in.</p>
+        </div>
+    );
+}
